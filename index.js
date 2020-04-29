ae45936ccf836486929a83329d0fc416f18be4f8
caicindy87 <caicindy87@gmail.com>
2020-04-28 18:24:54 -0700

    Done.

--- index.js
+++ index.js
@@ -1,23 +1,18 @@
+// Callback function needs to return either true or false
+// If returns true, the new array keeps that element
+// If returns false, element is filtered out
+// So, don't need to use if statement in the function
+
 function findMatching(drivers, string) {
-  return drivers.filter((driver) => {
-    if (driver === string || driver.toLowerCase() === string.toLowerCase()) {
-      return driver;
-    }
-  });
+  return drivers.filter(
+    (driver) => driver.toLowerCase() === string.toLowerCase()
+  );
 }
 
 function fuzzyMatch(drivers, string) {
-  return drivers.filter((driver) => {
-    if (driver.startsWith(string)) {
-      return driver;
-    }
-  });
+  return drivers.filter((driver) => driver.startsWith(string));
 }
 
 function matchName(drivers, string) {
-  return drivers.filter((driver) => {
-    if (driver.name === string) {
-      return driver;
-    }
-  });
+  return drivers.filter((driver) => driver.name === string);
 }
