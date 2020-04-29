4c97e2e215c40b84fab5826c48c98c11313b9f50
caicindy87 <caicindy87@gmail.com>
2020-04-28 18:18:28 -0700

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,23 @@
-// Code your solution here
+function findMatching(drivers, string) {
+  return drivers.filter((driver) => {
+    if (driver === string || driver.toLowerCase() === string.toLowerCase()) {
+      return driver;
+    }
+  });
+}
+
+function fuzzyMatch(drivers, string) {
+  return drivers.filter((driver) => {
+    if (driver.startsWith(string)) {
+      return driver;
+    }
+  });
+}
+
+function matchName(drivers, string) {
+  return drivers.filter((driver) => {
+    if (driver.name === string) {
+      return driver;
+    }
+  });
+}
